adf3ffd2ed33ee5456438facbcc42b93356decb3
David Kossoglyad <david@logoui.co.il>
2016-01-08 15:26:24 +0200

    GPS Connection is done

--- Sources/ScreamIt/ScreamIt/MainPage.xaml.cs
+++ Sources/ScreamIt/ScreamIt/MainPage.xaml.cs
@@ -1,17 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+using System.Globalization;
+using Windows.Devices.Geolocation;
 using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -20,11 +10,23 @@ namespace ScreamIt
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class MainPage
     {
         public MainPage()
         {
-            this.InitializeComponent();
+            InitializeComponent();
+        }
+
+        private async void MainPage_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            Geolocator geolocator = new Geolocator ();
+            var gps = await geolocator.GetGeopositionAsync();
+            
+            Latitude.Text = gps.Coordinate.Latitude.ToString(CultureInfo.CurrentCulture);
+            Longitude.Text = gps.Coordinate.Longitude.ToString(CultureInfo.CurrentCulture);
+
+            // Subscribe to the StatusChanged event to get updates of location status changes.
+
         }
     }
 }
